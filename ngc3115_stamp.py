069841b889d3386a4e243dc745fe7f4596ddf120
Marcos Tidball <47951223+zysymu@users.noreply.github.com>
2020-06-10 23:42:07 -0300

    Add files via upload

--- ngc3115_stamp.py
+++ ngc3115_stamp.py
@@ -80,91 +80,57 @@ def get_candidate(input_file, output_file, ra, dec, size):
 ###########################################
 
 
-def make_pieces(img_norm, cutout_size, border_size, ps, mzero, sizethresh, dir_name):
+def make_pieces(img_norm, cutout_size, overlap_size, ps, mzero, sizethresh, dir_name):
     """
 
     Parameters
     -------
     img_norm = 2D numpy array
 
-    cutout_size = int - size of the cutout
+    cutout_size = int / size of the cutout
 
-    border_size = float - size of the border, from 0. to 1. (times cutout_size)
+    overlap_size = float / percentage of overlap (0. = completely new cutout, no overlap; 1. = same cutout, total overlap)
 
-    ps = float - pixel scale
+    ps = float / pixel scale
 
-    mzero = float - magnitude zero point
+    mzero = float / magnitude zero point
 
-    sizethresh = int - size threshold for detected sources
+    sizethresh = int / size threshold for detected sources
 
-    dir_name = str - name of the directory where the stamps are going to be stored
+    dir_name = str / name of the directory where the stamps are going to be stored
     """
 
-    """
-    borders
-    """
-    
     #os.mkdir(dir_name)
     
     # stamps
     cutout = (cutout_size, cutout_size) 
-    num_images_per_line = ceil(img_norm.shape[0]/cutout_size)
-
-    # borders
-    border_vertical = (cutout_size, cutout_size*border_size)
-    border_horizontal = (cutout_size*border_size, cutout_size)
-
+    overlap = cutout_size * (1-overlap_size) # if cutout_size = 500 px, setting overlap = 0.2 would leave 100 pixels overlapping 
+    num_images_per_line = ceil(img_norm.shape[0]/overlap)
+    print(num_images_per_line)
+    
     row_pos = ceil(cutout_size/2)
-    border_row_pos = cutout_size
 
     frames = []
     
     for i in range(num_images_per_line): # changes the row
         col_pos = ceil(cutout_size/2)
-        border_col_pos = cutout_size
 
         for j in range(num_images_per_line): # changes the column
             cen = (row_pos, col_pos)
-            border_cen_vertical = (row_pos, border_col_pos)
-            border_cen_horizontal = (border_row_pos, col_pos)
-            
+
             # cutout
             small = extract_array(img_norm, cutout, cen)
             EllipseBBox(small, ps, mzero, sizethresh).show_stamps(title="center = " + str(cen))
             #df = EllipseBBox(small, ps, mzero, sizethresh).save_stamps(os.path.join(dir_name,str(cen)))
             #frames.append(df)
 
-            # right border
-            if j != num_images_per_line: # excludes the edge scenario
-                small = extract_array(img_norm, border_vertical, border_cen_vertical)
-                _, slices_small = overlap_slices(img_norm.shape, small.shape, border_cen_vertical)
-                EllipseBBox(small[slices_small], ps, mzero, sizethresh).show_stamps(title="border center = " + str(border_cen_vertical))
-                #df = EllipseBBox(small[slices_small], ps, mzero, sizethresh).save_stamps(os.path.join(dir_name,str(border_cen_vertical)))
-                #frames.append(df)
-
-            # upper border
-            if i != num_images_per_line:
-                small = extract_array(img_norm, border_horizontal, border_cen_horizontal)
-                _, slices_small = overlap_slices(img_norm.shape, small.shape, border_cen_horizontal)
-                EllipseBBox(small[slices_small], ps, mzero, sizethresh).show_stamps(title="border center = " + str(border_cen_horizontal))
-                #df = EllipseBBox(small[slices_small], ps, mzero, sizethresh).save_stamps(os.path.join(dir_name,str(border_cen_horizontal)))
-                #frames.append(df)
-
-            col_pos += cutout_size
-            border_col_pos += cutout_size
-
-        row_pos += cutout_size
-        border_row_pos += cutout_size
-        break
+            col_pos += overlap
+            
+        row_pos += overlap
 
     #result = pd.concat(frames)
     #result.reset_index(drop=True, inplace=True)
     #result.to_csv(os.path.join(dir_name, "catalog.csv"), index=False)
-
-    """
-    overlap
-    """
-    
 
 
 # 300x300 image
@@ -183,8 +149,10 @@ sizethresh = 50
 fits_image_filename = os.path.join(folder,img) # path to .fits image
     
 hdul = fits.open(fits_image_filename, memmap=True)
-#img_norm = hdul[0].data 
 img_norm = hdul[5].data 
+print(img_norm.shape)
 hdul.close()
 
-make_pieces(img_norm, 500, 0.3, ps, mzero, sizethresh, "stamps")
+start_time = time.time()
+make_pieces(img_norm, 1000, 0.2, ps, mzero, sizethresh, "stamps")
+print("--- %s seconds ---" % (time.time() - start_time))
